1ba20c89c1f541067b4fa565abf8c2e3936c8ab7
Nick Yeung <nickjyeung@gmail.com>
2025-11-22 16:50:59 -0600

    akjsld

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nickjyeung/Desktop/MadHacks2025/venv"
+set -gx VIRTUAL_ENV "/Users/nickjyeung/Desktop/MadHacks2025/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
